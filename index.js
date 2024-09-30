d36e18248a2120bc6fca978accc1394c55e6bbb9
lkl-clayy <onichanjiji531@gmail.com>
2024-09-30 19:37:37 -0300

    scriptdescuento

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ alert("Compra Realizada Exitosamente")
 console.log(producto,precio,cantidad)
 console.log(operacion)
 alert("total:"+operacion)
+
+function DescuentoDiezPorciento (precio, cantidad) {
+    let  total = precio * cantidad;
+    if (cantidad >= 5) {
+        let descuento = total * 0.10;
+        total -= descuento; 
+    }
+
+    return total;
+}
+
+let totalcondescuento = DescuentoDiezPorciento(precio, cantidad);
+
+console.log("Total a Pagar: $" + totalcondescuento)
+alert("Total A Pagar:$" + totalcondescuento);
